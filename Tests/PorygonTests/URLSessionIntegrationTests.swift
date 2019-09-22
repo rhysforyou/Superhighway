fe444f34b3ac2c29dd4a5bfcbfe4fbc4ea1f4b49
Rhys Powell <rbys@rpowell.me>
2019-09-22 18:43:06 +1000

    fix tests

--- Tests/PorygonTests/URLSessionIntegrationTests.swift
+++ Tests/PorygonTests/URLSessionIntegrationTests.swift
@@ -10,7 +10,11 @@ final class URLSessionIntegrationTests: XCTestCase {
     override func tearDown() {
         super.tearDown()
         URLProtocol.unregisterClass(HTTPStubURLProtocol.self)
+        subscriber = nil
     }
+    
+    
+    private var subscriber: Any?
 
     func testDataTaskRequest() throws {
         let url = URL(string: "http://www.example.com/example.json")!
@@ -19,18 +23,18 @@ final class URLSessionIntegrationTests: XCTestCase {
 
         let endpoint = Endpoint<[Person]>(json: .get, url: url)
         let expectation = self.expectation(description: "Stubbed network call")
-
-        let task = URLSession.shared.load(endpoint) { result in
-            switch result {
-            case let .success(payload):
+        
+        subscriber = endpoint.publisher()
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error):
+                    XCTFail(String(describing: error))
+                case .finished:
+                    expectation.fulfill()
+                }
+            }, receiveValue: { payload in
                 XCTAssertEqual([Person(name: "Alice"), Person(name: "Bob")], payload)
-                expectation.fulfill()
-            case let .failure(error):
-                XCTFail(String(describing: error))
-            }
-        }
-
-        task.resume()
+            })
 
         wait(for: [expectation], timeout: 1)
     }
