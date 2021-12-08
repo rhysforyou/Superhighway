2d653f842c1d4e1c87039b02f04c3d95922a7e14
Rhys Powell <rhys@rpowell.me>
2021-12-08 14:44:06 +1100

    increase minimum version requirement

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Superhighway",
-    platforms: [.iOS(.v13), .watchOS(.v6), .tvOS(.v13), .macOS(.v10_15)],
+    platforms: [.iOS(.v15), .watchOS(.v8), .tvOS(.v15), .macOS(.v12)],
     products: [
         .library(
             name: "Superhighway",
